e44d02f03d71ef025532390733e3c73746dc708b
theassetsquare <theassetsquare@gmail.com>
2026-03-01 04:03:56 +0000

    Auto update 2026-03-01 04:03:56 UTC

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://week1-6m5.pages.dev',
@@ -7,6 +6,5 @@ export default defineConfig({
   outDir: './dist',
   build: {
     format: 'directory'
-  },
-  integrations: [sitemap()]
+  }
 });
