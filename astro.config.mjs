3fe0986408726322705d474cf68e2686e3fb82cd
theassetsquare <theassetsquare@gmail.com>
2026-03-30 07:22:02 +0000

    Auto update 2026-03-30 07:22:02 UTC

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://week1-6m5.pages.dev',
+  base: '/',
   output: 'static',
   outDir: './dist',
   build: {
