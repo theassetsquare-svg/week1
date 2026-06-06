604713b02ad66e9cec03ca26325adfc2e0f769a5
theassetsquare <theassetsquare@gmail.com>
2026-06-06 21:01:40 +0000

    Auto update 2026-06-06 21:01:40 UTC

--- src/data/venues.ts
+++ src/data/venues.ts
@@ -2,7 +2,7 @@ import type { Venue } from './types';
 export type { Venue } from './types';
 export { TYPE_LABELS, TYPE_URLS } from './types';
 
-export const MAIN_SITE_URL = 'https://ilsanroom.pages.dev';
+export const MAIN_SITE_URL = 'https://nolcool.com';
 export const SITE_URL = 'https://week1-6m5.pages.dev';
 export const SLOGAN = '새벽까지 멈추지 않는 밤';
 export const SITE_NAME = '놀쿨';
